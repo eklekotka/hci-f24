0bf629eb29c4bd943d1d257fa942095df8467c09
Elizabeth McDowell <mcdowell.e@northeastern.edu>
2024-11-15 15:29:17 -0500

    welcome and login for existing user

--- frontend/src/pages/loginPage/loginPage.tsx
+++ frontend/src/pages/loginPage/loginPage.tsx
@@ -1,43 +1,39 @@
 import React, { useState, FormEvent } from 'react';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom'; 
-import './loginPage.css'; 
-import { useStudent } from '../../components/useStudent/useStudent'; 
+import { useNavigate } from 'react-router-dom';
+import './loginPage.css';
+import { useStudent } from '../../components/useStudent/useStudent';
 
 const LoginPage: React.FC = () => {
-  console.log("Login Page rendered?");
-
-  const { updateStudent } = useStudent(); 
+  const { updateStudent } = useStudent();
 
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [error, setError] = useState<string>('');
   const [showPassword, setShowPassword] = useState<boolean>(false);
-  
-  const navigate = useNavigate(); 
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-  
-    // Mock login response
+
     const result = { id: '1', name: 'Test Student', username }; // Mocked successful login
     if (result.id) {
-      console.log('Login Successful');
-      updateStudent(result); // Saves the student information
-      navigate('/homePage'); // Navigate to student home page
+      updateStudent(result);
+      navigate('/calendar');
     } else {
       setPassword('');
       setError('Login Error');
-      console.log('Login Error');
     }
   };
-  
+
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
   return (
     <form onSubmit={handleSubmit} className="form-container">
+      <h1 className="form-title">Sign In</h1>
       <div className="input-group">
         <label>Username:</label>
         <input
@@ -56,18 +52,14 @@ const LoginPage: React.FC = () => {
             onChange={(e) => setPassword(e.target.value)}
             required
           />
-        </div>
-        <div className="eye-icon" onClick={togglePasswordVisibility}>
-          {showPassword ? <FaEyeSlash /> : <FaEye />}
+          <div className="eye-icon" onClick={togglePasswordVisibility}>
+            {showPassword ? <FaEyeSlash /> : <FaEye />}
+          </div>
         </div>
       </div>
       <div className="forgot-password-error">
-        <div className="forgot-password">
-          <a href="/forgot-password">Forgot Password?</a>
-        </div>
-        <div className="error-message">
-          {error && <span>{error}</span>}
-        </div>
+        <a href="/forgot-password" className="forgot-password">Forgot Password?</a>
+        {error && <span className="error-message">{error}</span>}
       </div>
       <button className="submit-button" type="submit">Sign In</button>
     </form>
